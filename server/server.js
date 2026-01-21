19cc39f7db7622a9cdfe3d90a5df50a35b3e8a26
Sonika Tang <sonikatang630@gmail.com>
2026-01-22 06:24:24 +0700

    Comment out static asset serving in production

--- server/server.js
+++ server/server.js
@@ -22,14 +22,14 @@ app.post('/api/notify', async (req, res) => {
   res.json(result);
 });
 
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+// // Serve static assets in production
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
+//   });
+// }
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
